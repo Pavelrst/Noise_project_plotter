e7dd665747a915547dfbaf8a92bcf18ebcd215e3
Pavelrst <pavelr@campus.technion.ac.il>
2019-06-07 01:41:47 +0300

    first n epochs added- how many epochs do you want to plot?

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import matplotlib.pyplot as plt
 import os
 import csv
 import pandas as pd
+import numpy as np
 
+FIRST_N_EPOCHS = 50 # how many epochs do you want to plot?
 
 def main():
     rootdir = 'C:\\Users\\Pavel\\Desktop\\Noise_project_plotter\\results'
@@ -23,8 +25,14 @@ def main():
                         data = pd.read_csv(file_dir)
                         epochs_list = data['epoch']
                         val_error1_list = data['val_error1']
+
+                        if len(epochs_list) > FIRST_N_EPOCHS:
+                            epochs_list = epochs_list[:FIRST_N_EPOCHS]
+                            val_error1_list = val_error1_list[:FIRST_N_EPOCHS]
+
+                        min_err = np.min(val_error1_list)
                         val_error5_list = data['val_error5']
-                        label = dir
+                        label = dir + ': error=' + str(np.round(100*min_err,1)) + '% , acc=' + str(np.round(100*(1-min_err),1)) + '%'
                         plt.plot(epochs_list, val_error1_list, label=label)
 
     plt.xlabel('epochs')
